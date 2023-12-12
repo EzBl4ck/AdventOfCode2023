b40f8d8d1bcef5ee4c8ae118b4f494fe10e51b64
Federico <federico.capoccia@proton.me>
2023-12-12 19:17:27 +0100

    finished day 9

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,11 +1,75 @@
 advent_of_code::solution!(9);
 
-pub fn part_one(input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<isize> {
+    let mut total = 0;
+    for line in input.lines() {
+        let mut values: Vec<Vec<isize>> = Vec::new();
+        values.push(
+            line.split_whitespace()
+                .map(|s| s.parse().unwrap())
+                .collect(),
+        );
+
+        while !values.last().unwrap().iter().all(|n| *n == 0) {
+            values.push({
+                let last_values = values.last().unwrap();
+                let mut new_values = Vec::new();
+
+                for i in 1..last_values.len() {
+                    new_values.push(last_values[i] - last_values[i - 1]);
+                }
+
+                new_values
+            })
+        }
+
+        let mut partial = 0;
+        for i in (0..values.len()).rev() {
+            values[i].push(partial);
+
+            match i {
+                0 => total += partial,
+                _ => partial += values[i - 1].last().unwrap(),
+            }
+        }
+    }
+    Some(total)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<isize> {
+    let mut total = 0;
+    for line in input.lines() {
+        let mut values: Vec<Vec<isize>> = Vec::new();
+        values.push(
+            line.split_whitespace()
+                .map(|s| s.parse().unwrap())
+                .collect(),
+        );
+
+        while !values.last().unwrap().iter().all(|n| *n == 0) {
+            values.push({
+                let last_values = values.last().unwrap();
+                let mut new_values = Vec::new();
+
+                for i in 1..last_values.len() {
+                    new_values.push(last_values[i] - last_values[i - 1]);
+                }
+
+                new_values
+            })
+        }
+
+        let mut partial = 0;
+        for i in (0..values.len()).rev() {
+            values[i].insert(0, partial);
+
+            match i {
+                0 => total += partial,
+                _ => partial = values[i - 1].first().unwrap() - partial,
+            }
+        }
+    }
+    Some(total)
 }
 
 #[cfg(test)]
@@ -15,12 +79,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(114));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2));
     }
 }
